455f91fd597a0d71400b0f557f2c1edd2d5c9777
Taras Kolodny <trskldn@gmail.com>
2016-05-10 01:43:53 +0300

    jsdom

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,9 +1,9 @@
 var jsdom = require("jsdom").jsdom;
-var jquery = require("jquery");
-
 var doc = jsdom('<!doctype html><html><body></body></html>');
 var win = doc.defaultView;
+var jquery = require("jquery")(win);
 
+global.window.$ = jquery;
 global.document = doc;
 global.window = win;
 
